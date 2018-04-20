4765e96dbc9729cbd6692bde716534bd3694048c
Aayush Mittal <aayush.mittal@squintmetrics.com>
2018-04-20 22:42:25 +0000

    using short array according to code standards recommended.

--- src/Generators/Ics.php
+++ src/Generators/Ics.php
@@ -8,13 +8,13 @@ use Spatie\CalendarLinks\Generator;
 class Ics implements Generator {
 
   public function generate(Link $link): string {
-    $url = array('data:text/calendar;charset=utf8,',
+    $url = ['data:text/calendar;charset=utf8,',
       'BEGIN:VCALENDAR',
       'VERSION:2.0',
       'BEGIN:VEVENT',
       'DTSTART:' . $link->from->format('Ymd\THis'),
       'DTEND:' . $link->to->format('Ymd\THis'),
-      'SUMMARY:' . $link->title);
+      'SUMMARY:' . $link->title];
 
     if ($link->description) {
       $url[] = 'DESCRIPTION:' . $link->description;
