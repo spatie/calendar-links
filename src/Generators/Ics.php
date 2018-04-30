da96e0df5abd511affccd0ad24deb13f65247d94
Freek Van der Herten <freek@spatie.be>
2018-04-30 07:59:41 +0000

    Apply fixes from StyleCI

--- src/Generators/Ics.php
+++ src/Generators/Ics.php
@@ -5,28 +5,29 @@ namespace Spatie\CalendarLinks\Generators;
 use Spatie\CalendarLinks\Link;
 use Spatie\CalendarLinks\Generator;
 
-class Ics implements Generator {
-
-  public function generate(Link $link): string {
-    $url = ['data:text/calendar;charset=utf8,',
+class Ics implements Generator
+{
+    public function generate(Link $link): string
+    {
+        $url = ['data:text/calendar;charset=utf8,',
       'BEGIN:VCALENDAR',
       'VERSION:2.0',
       'BEGIN:VEVENT',
-      'DTSTART:' . $link->from->format('Ymd\THis'),
-      'DTEND:' . $link->to->format('Ymd\THis'),
-      'SUMMARY:' . $link->title];
+      'DTSTART:'.$link->from->format('Ymd\THis'),
+      'DTEND:'.$link->to->format('Ymd\THis'),
+      'SUMMARY:'.$link->title, ];
 
-    if ($link->description) {
-      $url[] = 'DESCRIPTION:' . $link->description;
+        if ($link->description) {
+            $url[] = 'DESCRIPTION:'.$link->description;
+        }
+        if ($link->address) {
+            $url[] = 'LOCATION:'.str_replace(',', '', $link->address);
+        }
+
+        $url[] = 'END:VEVENT';
+        $url[] = 'END:VCALENDAR';
+        $redirectLink = implode("\n", $url);
+
+        return $redirectLink;
     }
-    if ($link->address) {
-      $url[] = 'LOCATION:' . str_replace(',', '', $link->address);
-    }
-
-    $url[] = 'END:VEVENT';
-    $url[] = 'END:VCALENDAR';
-    $redirectLink = join("\n", $url);
-
-    return $redirectLink;
-  }
 }
