1f363dc8b9c735e118ee6a3cc5bbcacd9fad0154
Alex lptn <web.lapateen@gmail.com>
2018-10-13 21:20:11 +0300

    Add tests for WebOutlook generator

---
+++ tests/Generators/__snapshots__/WebOutlookGeneratorTest__it_can_generate_a_web_outlook_allDay_link__1.php
@@ -0,0 +1,3 @@
+<?php
+
+return 'https://outlook.live.com/owa/?path=/calendar/action/compose&rru=addevent&startdt=20180201T090000&enddt=20180201T090000&allday=true&subject=Birthday&body=With+clowns+and+stuff&location=Party+Lane+1A%2C+1337+Funtown';
