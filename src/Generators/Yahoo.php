464e98d047e14f8d3e9c70c3360b951fc062876b
Purvesh <ind.purvesh@gmail.com>
2018-11-02 12:24:51 +1300

    Fixed Issue #35 Yahoo Generator broken
    Just create a DateTime Object to figure our the time difference.

--- src/Generators/Yahoo.php
+++ src/Generators/Yahoo.php
@@ -9,11 +9,13 @@ class Yahoo implements Generator
 {
     public function generate(Link $link): string
     {
+        $from = new DateTime($link->from);
+        $to = new DateTime($link->to);
         $url = 'https://calendar.yahoo.com/?v=60&view=d&type=20';
 
         $url .= '&title='.urlencode($link->title);
-        $url .= '&st='.$link->from->format('Ymd\THis\Z');
-        $url .= '&dur='.date_diff($link->from, $link->to)->format('%H%I');
+        $url .= '&st='.$from->format('Ymd\THis\Z');
+        $url .= '&dur='.date_diff($from, $to)->format("%H%I");
 
         if ($link->description) {
             $url .= '&desc='.urlencode($link->description);
