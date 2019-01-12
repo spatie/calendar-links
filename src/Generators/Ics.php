8d0c4574917221030b72c7ca22effc3f68138b65
Alex lptn <web.lapateen@gmail.com>
2019-01-12 13:09:05 +0300

    Use CRLF sequence for line breaks for .ics file
    See https://tools.ietf.org/html/rfc5545.html#section-3.1

--- src/Generators/Ics.php
+++ src/Generators/Ics.php
@@ -39,7 +39,7 @@ class Ics implements Generator
 
         $url[] = 'END:VEVENT';
         $url[] = 'END:VCALENDAR';
-        $redirectLink = implode('%0A', $url);
+        $redirectLink = implode('%0d%0a', $url);
 
         return 'data:text/calendar;charset=utf8,'.$redirectLink;
     }
