433657cf151d8d8ec63d4dbe21281eeee3b8d7a3
Alies Lapatsin <web.lapateen@gmail.com>
2019-05-23 16:10:23 +0300

    #60 Use short format for all outlook all-day links

--- tests/Generators/__snapshots__/WebOutlookGeneratorTest__it_can_generate_a_web_outlook_allDay_link__1.php
+++ tests/Generators/__snapshots__/WebOutlookGeneratorTest__it_can_generate_a_web_outlook_allDay_link__1.php
@@ -1,1 +1,1 @@
-<?php return 'https://outlook.live.com/owa/?path=/calendar/action/compose&rru=addevent&startdt=20180201T090000&enddt=20180201T090000&allday=true&subject=Birthday&body=With+balloons%2C+clowns+and+stuff%0ABring+a+dog%2C+bring+a+frog&location=Party+Lane+1A%2C+1337+Funtown';
+<?php return 'https://outlook.live.com/owa/?path=/calendar/action/compose&rru=addevent&startdt=20180201&enddt=20180201&allday=true&subject=Birthday&body=With+balloons%2C+clowns+and+stuff%0ABring+a+dog%2C+bring+a+frog&location=Party+Lane+1A%2C+1337+Funtown';
