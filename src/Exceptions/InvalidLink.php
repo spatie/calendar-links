762c59f134002bc97e095811b0bda365d48d742a
Alies Lapatsin <web.lapateen@gmail.com>
2020-07-07 17:00:43 +0300

    Improve exception message

--- src/Exceptions/InvalidLink.php
+++ src/Exceptions/InvalidLink.php
@@ -3,12 +3,14 @@
 namespace Spatie\CalendarLinks\Exceptions;
 
 use DateTimeInterface;
-use Exception;
+use InvalidArgumentException;
 
-class InvalidLink extends Exception
+class InvalidLink extends InvalidArgumentException
 {
+    private const DATETIME_FORMAT = 'Y-m-d H:i:s';
+
     public static function invalidDateRange(DateTimeInterface $to, DateTimeInterface $from): self
     {
-        return new self("`{$to->format('YMD His')}` must be greater than `{$from->format('YMD His')}`");
+        return new self("TO time (`{$to->format(self::DATETIME_FORMAT)}`) must be greater than FROM time (`{$from->format(self::DATETIME_FORMAT)}`)");
     }
 }
