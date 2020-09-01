5273b53f9e8a076eba552232fa89d42c6d50c5a7
Alies Lapatsin <web.lapateen@gmail.com>
2020-09-01 14:48:08 +0300

    #99 Update spatie/phpunit-snapshot-assertions

--- tests/Generators/__snapshots__/WebOutlookGeneratorTest__it_can_generate_a_short_event_link__1.php
+++
@@ -1,1 +0,0 @@
-<?php return 'https://outlook.live.com/calendar/deeplink/compose?path=/calendar/action/compose&rru=addevent&startdt=2018-02-01T09:00:00Z&enddt=2018-02-01T18:00:00Z&subject=Birthday&body=With+balloons%2C+clowns+and+stuff%0ABring+a+dog%2C+bring+a+frog&location=Party+Lane+1A%2C+1337+Funtown';
