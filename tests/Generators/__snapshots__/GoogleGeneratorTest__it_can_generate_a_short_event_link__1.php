23cc74a72bb59e7f9fa9ee2d1436c189f38a5036
Alies Lapatsin <web.lapateen@gmail.com>
2020-09-01 16:38:27 +0300

    #99 Revert phpunit-snapshot-assertions update to support php 7.2

---
+++ tests/Generators/__snapshots__/GoogleGeneratorTest__it_can_generate_a_short_event_link__1.php
@@ -0,0 +1,1 @@
+<?php return 'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20180201T090000/20180201T180000&ctz=UTC&text=Birthday&details=With+balloons%2C+clowns+and+stuff%0ABring+a+dog%2C+bring+a+frog&location=Party+Lane+1A%2C+1337+Funtown&sprop=&sprop=name:';
---
+++ tests/Generators/__snapshots__/WebOutlookGeneratorTest__it_can_generate_a_single_day_allday_event_link__1.php
@@ -0,0 +1,1 @@
+<?php return 'https://outlook.live.com/calendar/deeplink/compose?path=/calendar/action/compose&rru=addevent&startdt=2018-02-01&enddt=2018-02-02&allday=true&subject=Birthday&body=With+balloons%2C+clowns+and+stuff%0ABring+a+dog%2C+bring+a+frog&location=Party+Lane+1A%2C+1337+Funtown';
