419b9f2b85f46dcec1014c2ee79b446c812b5dd9
Alies Lapatsin <web.lapateen@gmail.com>
2022-12-11 03:48:01 +0100

    ICS: Remove `X-ALT-DESC`, use `DESCRIPTION` with strip_tags instead

--- src/Generators/Ics.php
+++ src/Generators/Ics.php
@@ -51,7 +51,6 @@ class Ics implements Generator
 
         if ($link->description) {
             $url[] = 'DESCRIPTION:'.strip_tags($link->description);
-            $url[] = 'X-ALT-DESC;FMTTYPE=text/html:'.$this->escapeString($link->description);
         }
         if ($link->address) {
             $url[] = 'LOCATION:'.$this->escapeString($link->address);
