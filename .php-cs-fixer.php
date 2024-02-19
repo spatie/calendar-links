14bc6d5d432281f9b0097849999fd8197e99cd0e
Alies Lapatsin <web.lapateen@gmail.com>
2024-02-20 00:17:28 +0530

    Use PSR12

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,13 +6,12 @@ $finder = PhpCsFixer\Finder::create()
         __DIR__ . '/tests',
     ])
     ->name('*.php')
-    ->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR2' => true,
+        '@PSR12' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
         'no_unused_imports' => true,
