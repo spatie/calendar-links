5ad4ee4f3e227b59b50ab414ed3af830396c4903
Alies Lapatsin <5278175+alies-dev@users.noreply.github.com>
2026-08-20 16:37:17 +0200

    refactor: add type info

--- src/Generators/WebOffice.php
+++ src/Generators/WebOffice.php
@@ -7,7 +7,7 @@ namespace Spatie\CalendarLinks\Generators;
 final class WebOffice extends BaseOutlook
 {
     /** @var non-empty-string */
-    private const BASE_URL = 'https://outlook.cloud.microsoft/calendar/deeplink/compose?path=/calendar/action/compose&rru=addevent';
+    private const string BASE_URL = 'https://outlook.cloud.microsoft/calendar/deeplink/compose?path=/calendar/action/compose&rru=addevent';
 
     /** @inheritDoc */
     #[\Override]
